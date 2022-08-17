52e0f78695addab661bbb4fd9e799b360830297c
Rishabh Raghuwanshi <rishabh@Rishabhs-MacBook-Pro.local>
2022-08-17 11:12:33 +0530

    second commit

--- src/main/java/com/consultAdd/service/EmployeeService.java
+++ src/main/java/com/consultAdd/service/EmployeeService.java
@@ -13,22 +13,32 @@ public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    public List<Employee> getEmployees()
-    {
+    public List<Employee> getEmployees() {
         return employeeRepository.findAll();
     }
 
-    public String saveEmployee(Employee employee)
-    {
-        if(employeeRepository.existsById(employee.getId()))
-        {
+    public String saveEmployee(Employee employee) {
+        if (employeeRepository.existsById(employee.getId())) {
             return "Employee already exists";
-        }
-        else
-        {
+        } else {
             employeeRepository.save(employee);
             return "Save successfully";
         }
-
+    }
+    public String putEmployee(Employee employee) {
+        if (employeeRepository.existsById(employee.getId())) {
+            employeeRepository.save(employee);
+            return "Data Updated successfully";
+        } else {
+            return "Employee doesn't exists";
+        }
+    }
+    public String deleteEmployee(Employee employee) {
+        if (employeeRepository.existsById(employee.getId())) {
+            employeeRepository.delete(employee);
+            return "Data deleted successfully";
+        } else {
+            return "Employee doesn't exists";
+        }
     }
 }
